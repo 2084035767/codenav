21b2c789592a49a94fd9f492a3a6e77e062d20a0
zs <2084035767@qq.com>
2023-12-13 11:12:20 +0800

    Update: somethings

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,3 +1,3 @@
 import type { DefaultTheme } from 'vitepress'
 
-export const nav: DefaultTheme.Config['nav'] = [{ text: '前端导航', link: '/nav/' }]
+export const nav: DefaultTheme.Config['nav'] = [{ text: '编程导航', link: '/nav/' }]
